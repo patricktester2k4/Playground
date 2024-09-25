bb75ad18f8338fd28c403a6079eeb20e2ac132e1
Patrick <160852004+patricktester2k4@users.noreply.github.com>
2024-09-25 11:48:10 +0000

    fixig the prompt after giving invalid value in prompt

--- Pyramid.py
+++ Pyramid.py
@@ -1,31 +1,38 @@
-def mario_pyramid(height):
-  """
-  Prints a single-sided Mario-style pyramid of '#' characters with the specified height.
-
-  Args:
-    height: The height of the pyramid (between 1 and 8).
-  """
-
-  if height < 1 or height > 8:
-    print("Invalid input. Please enter a height between 1 and 8.")
-    print("1. Input the correct value")
-    print("2. Exit")
-    choice = input("Enter your choice: ")
-    if choice == '1':
-      height = int(input("Enter the height of the pyramid (1-8): "))
-      mario_pyramid(height)
-    elif choice == '2':
-      return
-    else:
-      print("Invalid choice.")
-      return
-
+def build_pyramid(height):
+  """Builds a pyramid of '#' characters."""
   for i in range(1, height + 1):
-    print(" " * (height - i), end="")  # Print spaces
-    print("#" * i)  # Print '#' characters
+    print("#" * i)
 
-# Get user input for the pyramid height
-height = int(input("Enter the height of the pyramid (1-8): "))
-
-# Call the function to print the pyramid
-mario_pyramid(height)
+while True:
+  try:
+    height = int(input("Enter the height of the pyramid (1-8): "))
+    if 1 <= height <= 8:
+      build_pyramid(height)
+      while True:
+        choice = input("1. Play again\n2. Exit\nChoose an option: ")
+        if choice == '1':
+          break
+        elif choice == '2':
+          exit()
+        else:
+          print("Invalid choice. Please enter 1 or 2.")
+    else:
+      print("Invalid input. Please enter a number between 1 and 8.")
+      while True:
+        retry_choice = input("1. Try again\n2. Exit\nChoose an option: ")
+        if retry_choice == '1':
+          break
+        elif retry_choice == '2':
+          exit()
+        else:
+          print("Invalid choice. Please enter 1 or 2.")
+  except ValueError:
+    print("Invalid input. Please enter a number.")
+    while True:
+      retry_choice = input("1. Try again\n2. Exit\nChoose an option: ")
+      if retry_choice == '1':
+        break
+      elif retry_choice == '2':
+          exit()
+      else:
+        print("Invalid choice. Please enter 1 or 2.")
